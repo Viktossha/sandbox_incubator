a80e43c9364ff631922ad0aedc399a41dd3223cc
v.petrova <vikapetrova8@gmail.com>
2023-10-17 20:33:20 +0300

    05 js fix

--- js/05.js
+++ js/05.js
@@ -8,7 +8,7 @@ function lovefunc(flower1, flower2) {
 
 //Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 var countSheep = function (num) {
-    string = ''
+    let string = ''
     for (var i = 1; i <= num; i++) {
         string += i + ' sheep...'
     }

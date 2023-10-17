a7edd384609d011ce1e73397ebf587c279b2007b
v.petrova <vikapetrova8@gmail.com>
2023-10-17 09:56:48 +0300

    04 js

--- js/04.js
+++ js/04.js
@@ -14,4 +14,6 @@ const ourDecimal = 5.7;
 let myDecimal = 1.2;
 
 const product = 2.0 * 2.5;
-const quotient = 4.4 / 2.0; // Change this line
+const quotient = 4.4 / 2.0;
+
+const remainder = 11 % 3;

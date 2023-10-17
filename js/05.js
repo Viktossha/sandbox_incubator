46a1a5977cfa694107431b1c34bada78c325c785
v.petrova <vikapetrova8@gmail.com>
2023-10-17 23:08:52 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -21,3 +21,10 @@ function solution(str) {
     let reverseArr = arr.reverse()
     return reverseArr.join('')
 }
+
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message:
+
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest'
+}

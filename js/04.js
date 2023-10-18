06c7ace49904f6840031df966803bb4d1a81cebc
v.petrova <vikapetrova8@gmail.com>
2023-10-18 14:41:05 +0300

    04 js

--- js/04.js
+++ js/04.js
@@ -85,3 +85,7 @@ myStr = "Hello World"; // Change this line
 const lastName = "Lovelace";
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1];

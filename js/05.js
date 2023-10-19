90865fd71bd7ebf12f42d688251d37e527e542f5
v.petrova <vikapetrova8@gmail.com>
2023-10-19 19:45:06 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -41,3 +41,15 @@ function abbrevName(name) {
     let secondLetter = name.split(" ")[1][0].toUpperCase()
     return firstLetter + '.' + secondLetter
 }
+
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+function grow(x) {
+    let result = 1;
+    if (x) {
+        for (let i = 0; i < x.length; i++) {
+            result *= x[i];
+        }
+    }
+    return result;
+}

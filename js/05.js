621022fdd0968e654c960898a89d08d1fc3da45b
v.petrova <vikapetrova8@gmail.com>
2023-10-19 19:57:40 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -53,3 +53,16 @@ function grow(x) {
     }
     return result;
 }
+
+//Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x) {
+    let result = [];
+    if (x) {
+        for (let i = 0; i < x.length; i++) {
+            result.push(x[i] * 2);
+        }
+    }
+    return result;
+}

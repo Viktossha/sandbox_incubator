67d26129c8909c33cbfeba90e3c9e9eea7691a47
v.petrova <vikapetrova8@gmail.com>
2023-10-19 22:33:32 +0300

    04 js

--- js/04.js
+++ js/04.js
@@ -126,3 +126,9 @@ let removedFromMyArray = myArray.shift();
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
 myArray.unshift(["Paul", 35]);
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();

cb96de592fbb43323787ac163ac0c559e844dfc3
v.petrova <vikapetrova8@gmail.com>
2023-10-20 12:51:18 +0300

    04 js

--- js/04.js
+++ js/04.js
@@ -352,6 +352,7 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
 function orderMyLogic(val) {
     if (val < 5) {
         return "Less than 5";
@@ -363,3 +364,23 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return 'Tiny';
+    } else if (num < 10) {
+        return 'Small';
+    } else if (num < 15) {
+        return 'Medium';
+    } else if (num < 20) {
+        return 'Large';
+    } else if (num >= 20) {
+        return 'Huge';
+    }
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+testSize(7);

24d7c4e2df04f8989fb3ea28b2008165827b2e29
v.petrova <vikapetrova8@gmail.com>
2023-10-20 18:29:51 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -159,3 +159,9 @@ function litres(time) {
     const litersPerHour = 0.5;
     return Math.floor(time * litersPerHour);
 }
+
+//Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+function booleanToString(b){
+    return b === true ? 'true' : 'false';
+}

772a7a7d05a1fd59301fda53d3e855dc65a61136
v.petrova <vikapetrova8@gmail.com>
2023-10-20 18:20:02 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -151,3 +151,11 @@ var summation = function (num) {
     }
     return res;
 }
+
+//Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+function litres(time) {
+    const litersPerHour = 0.5;
+    return Math.floor(time * litersPerHour);
+}

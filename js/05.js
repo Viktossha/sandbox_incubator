fda99cba4500337afc63e0a821930e9ac4707cfb
v.petrova <vikapetrova8@gmail.com>
2023-10-21 20:57:28 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -242,3 +242,8 @@ function positiveSum(arr) {
     }
     return res;
 }
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No';
+}

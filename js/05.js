dfdf64ef8d77aa9317d725c9ab29cda4f89e1b51
v.petrova <vikapetrova8@gmail.com>
2023-10-21 20:44:05 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -215,3 +215,8 @@ function repeatStr(n, s) {
 function repeatStr (n, s) {
     return s.repeat(n);
 }
+
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8 : number * 9;
+}

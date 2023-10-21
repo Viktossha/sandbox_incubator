49d990d619025474acb1d650b6ec5bf5419ea6e6
v.petrova <vikapetrova8@gmail.com>
2023-10-21 20:40:54 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -198,3 +198,15 @@ function makeNegative(num) {
 function removeChar(str) {
     return str.slice(1, -1);
 };
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+function repeatStr(n, s) {
+    let res = '';
+    for (let i = 0; i < n; i++) {
+        res += s;
+    }
+    return res;
+}

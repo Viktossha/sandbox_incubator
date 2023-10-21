6f5410fd4161c40fc45d9030369a5abd6897c69e
v.petrova <vikapetrova8@gmail.com>
2023-10-21 20:17:49 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -193,3 +193,8 @@ function areYouPlayingBanjo(name) {
 function makeNegative(num) {
     return num > 0 ? -num : num
 }
+
+//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+function removeChar(str) {
+    return str.slice(1, -1);
+};

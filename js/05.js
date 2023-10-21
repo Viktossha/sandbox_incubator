be4881cd9c2bc85f5f119a436cfd43a79318efd7
v.petrova <vikapetrova8@gmail.com>
2023-10-21 20:55:53 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -229,3 +229,16 @@ function opposite(number) {
 function opposite(number) {
     return(-number);
 }
+
+//You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {
+    let res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            res += arr[i];
+        }
+    }
+    return res;
+}

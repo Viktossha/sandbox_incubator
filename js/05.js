987c69b542c58320ea2116772e3690373494807b
v.petrova <vikapetrova8@gmail.com>
2023-10-23 09:14:53 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -333,7 +333,7 @@ function findAverage(array) {
 // wait is the number of people waiting to get on to the bus excluding the driver.
 // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 function enough(cap, on, wait) {
-    return cap - (on+wait) > 0 ? 0 : Math.abs(cap - (on+wait));
+    return cap - (on + wait) > 0 ? 0 : Math.abs(cap - (on + wait));
 }
 
 //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
@@ -349,6 +349,19 @@ function sum(numbers) {
     "use strict";
     return numbers.reduce(function (sum, elem) {
         return sum + elem;
-    },0)
-
+    }, 0)
 };
+
+
+//This function should test if the factor is a factor of base.
+// Return true if it is a factor or false if it is not.
+// About factors
+// Factors are numbers you can multiply together to get another number.
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+// Note: base is a non-negative number, factor is a positive number.
+function checkForFactor (base, factor) {
+    return base % factor === 0;
+}

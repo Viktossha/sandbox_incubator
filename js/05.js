a9e0eb218203004b430a8356e100dd1e037590cb
v.petrova <vikapetrova8@gmail.com>
2023-10-25 22:21:00 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -373,3 +373,17 @@ function checkForFactor (base, factor) {
 function cockroachSpeed(s) {
     return Math.floor((s * 100000)/3600);
 }
+
+//Task
+// Create a method to see whether the string is ALL CAPS.
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+String.prototype.isUpperCase = function() {
+    return this == this.toUpperCase();
+}

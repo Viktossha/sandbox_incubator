09718bf4444b26a50b655a9f5852a4853ee94517
vika-hard-skill-reset <vikapetrova8@gmail.com>
2023-12-11 19:51:23 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -525,3 +525,16 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 function hoopCount (n) {
     return n > 9 ? "Great, now move on to tricks" : "Keep at it until you get it"
 }
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+// Note: input will never be an empty string
+function fakeBin(x){
+    let arr = Array.from(x);
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] > 4 ? newArr.push(1) : newArr.push(0);
+    }
+
+    return newArr.join('');
+}

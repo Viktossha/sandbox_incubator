c96cfe8193cff6269dc73bdcc6104c283d84b2a2
vika-hard-skill-reset <vikapetrova8@gmail.com>
2023-12-11 19:27:08 +0300

    05 js

--- js/05.js
+++ js/05.js
@@ -479,3 +479,45 @@ function past(h, m, s){
 function sumStr(a,b) {
     return String(Number(a) + Number(b));
 }
+
+Vika, [10.12.2023 21:01]
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+Vika, [10.12.2023 21:01]
+function sumStr(a,b) {
+    return String(Number(a) + Number(b));
+}
+
+// I have a cat and a dog.
+//     I got them at the same time as kitten/puppy. That was humanYears years ago.
+//     Return their respective ages now as [humanYears,catYears,dogYears]
+//
+// NOTES:
+//     humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears = 0
+    let dogYears = 0
+
+    if (humanYears === 1) {
+        catYears = dogYears = 15;
+    } else if (humanYears === 2) {
+        catYears = dogYears = 24;
+    } else if (humanYears > 2) {
+        catYears = 4 * (humanYears - 2) + 24 ;
+        dogYears = 5 * (humanYears - 2) + 24
+    }
+
+    return [humanYears,catYears,dogYears];
+}

9316647be0eace3bdd975864d1e49c84907ddf8d
vika-hard-skill-reset <vikapetrova8@gmail.com>
2023-12-11 19:40:51 +0300

    05 js

--- js/04.js
+++ js/04.js
@@ -589,3 +589,11 @@ function updateRecords(records, id, prop, value) {
 }
 
 updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}

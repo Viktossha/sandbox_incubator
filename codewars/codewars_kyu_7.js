81a396d5ed96d6f65fb0da6161c239c5e30dd97f
user <vikapetrova8@gmail.com>
2024-01-05 21:32:40 +0300

    codewars

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -16,3 +16,29 @@ var number = function (busStops) {
 
     return peopleOn - peopleOff
 }
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+//Examples input/output:
+//
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+
+    let arrO = 0
+    let arrX = 0
+
+    str.toLowerCase().split('').map((item, index) => {
+        if (item === ('o' || 'O')) {
+            arrO += 1
+        }
+        if (item === ('x' || 'X')) {
+            arrX += 1
+        }
+    })
+
+    return arrO === arrX
+}

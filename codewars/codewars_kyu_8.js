25abcb03e2e32ecc6e60134ae3a62db5c9d02997
user <vikapetrova8@gmail.com>
2024-01-12 22:13:39 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -601,3 +601,20 @@ function sumArray(array) {
 
     return sum
 }
+
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+
+function bmi(weight, height) {
+    let bmi = weight / (height ** 2)
+
+    if (bmi <= 18.5) {
+        return "Underweight"
+    } else if (bmi <= 25.0) {
+        return "Normal"
+    } else if (bmi <= 30.0) {
+        return "Overweight"
+    } else if (bmi > 30 ) {
+        return "Obese"
+    }
+}

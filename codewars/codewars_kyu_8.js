e4dbe30b74f1f44f4e5b135f862c200d191e6eee
user <vikapetrova8@gmail.com>
2024-01-13 19:02:56 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -636,3 +636,21 @@ function findNeedle(haystack) {
     //проще
     //return `found the needle at position ${haystack.indexOf('needle')}`;
 }
+
+//In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+//
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+//
+// Your function will be tested with pre-made examples as well as random ones.
+
+function findDifference(a, b) {
+    let newA = a.reduce(function (a, b) {
+        return a * b;
+    });
+
+    let newB = b.reduce(function (a, b) {
+        return a * b;
+    });
+
+    return Math.abs(newA - newB)
+}

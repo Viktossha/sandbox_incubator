2fa590d790bd663e754c915e04d993725f749ea8
Vika <vikapetrova8@gmail.com>
2024-01-20 21:57:07 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -722,3 +722,20 @@ function onlyOnceNum (nums) {
         }
     }
 }
+
+//For example:
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+    let res = []
+
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === 'T') {
+            res[i] = 'U'
+        } else {
+            res[i] = dna[i]
+        }
+    }
+    return res.join('')
+}

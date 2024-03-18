4d4f3bf3bb7f5187504861a00d92b67b265ed056
Vika <vikapetrova8@gmail.com>
2024-03-19 00:01:56 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -788,3 +788,10 @@ function findMultiples(integer, limit) {
 function hero(bullets, dragons){
     return (bullets/dragons) >= 2
 }
+
+//We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+function plural(n) {
+    return n !== 1
+}
+

c556b652422ad00924fe719a6eeadcec6482c476
Vika <vikapetrova8@gmail.com>
2024-04-01 21:38:36 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -945,3 +945,56 @@ function shortcut (string) {
 function getChar(c){
     return String.fromCharCode(c)
 }
+
+//The Task
+// Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+function greet(language) {
+    const greetings = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso",
+    }
+
+    const keys = Object.keys(greetings);
+    let str = ''
+    keys.filter(el => {
+        if (el === language) {
+            str = greetings[el]
+        } else {
+            str = 'Welcome'
+        }
+    })
+    return str
+}
+
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//
+// For example,
+//
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+//
+// Hint: Don't forget to check for bad values like null/undefined
+function countSheeps(sheep) {
+    return sheep.filter(el => el).length
+}

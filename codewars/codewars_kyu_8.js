81cbcc08b555201182953b0bb1c95c795d8ed9dc
Vika <vikapetrova8@gmail.com>
2024-04-03 21:59:16 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -998,3 +998,17 @@ function greet(language) {
 function countSheeps(sheep) {
     return sheep.filter(el => el).length
 }
+
+//Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+//
+// For example:
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+function between(a, b) {
+    let arr = []
+    for (let i = a; i <= b; i++) {
+        arr.push(i)
+    }
+    return arr
+}

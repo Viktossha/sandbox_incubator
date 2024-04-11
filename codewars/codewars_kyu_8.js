a6d199b93c90743c81cdbd016225841161d58100
Vika <vikapetrova8@gmail.com>
2024-04-11 11:41:39 +0300

    codewars Get the mean of an array

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1113,3 +1113,12 @@ function points(games) {
 
     return res
 }
+
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+//
+// Return the average of the given array rounded down to its nearest integer.
+//
+// The array will never be empty.
+function getAverage(marks){
+    return Math.round(marks.reduce(function (a, b) {return a + b}) / marks.length)
+}

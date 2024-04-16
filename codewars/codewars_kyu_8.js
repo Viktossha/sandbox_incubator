9c099d4a92457831ecb721e3649b477ee4be0fe0
Vika <vikapetrova8@gmail.com>
2024-04-16 21:50:24 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1151,3 +1151,11 @@ function warnTheSheep(queue) {
 
     return str
 }
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//
+// Return your answer as a number
+function sumMix(x){
+    let newArr = x.map(el => Number(el))
+    return newArr.reduce(function (a, b) { return a + b})
+}

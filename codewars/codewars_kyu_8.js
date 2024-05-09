b72fbb6bf94842a5333887b6d9701dc08b0143a9
Vika <vikapetrova8@gmail.com>
2024-05-09 13:38:35 +0300

    codewars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1227,3 +1227,15 @@ function monkeyCount(n) {
 function divisibleBy(numbers, divisor){
     return numbers.filter(n => n % divisor === 0)
 }
+
+//Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+//
+// Examples:
+//
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+function distinct(a) {
+    return  a.filter((item, index) => a.indexOf(item) === index);
+}

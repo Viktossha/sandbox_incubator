c58cd4783ab11d1d68a51b5e9eb1cf504b4b415c
Vika <vikapetrova8@gmail.com>
2024-05-18 11:45:13 +0300

    Sort and Star

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1276,3 +1276,14 @@ function mergeArrays(arr1, arr2) {
     let newArr = arr1.concat(arr2).sort((a, b) => a - b)
     return newArr.filter((el, index) => newArr.indexOf(el) === index)
 }
+
+//You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+//
+// The returned value must be a string, and have "***" between each of its letters.
+//
+// You should not remove or add elements from/to the array.
+function twoSort(s) {
+    return s.sort()[0].split('').join('***')
+    // let firstWorld = s.sort()[0]
+    // return firstWorld.split('').map((item, index) => index !== (firstWorld.length- 1) ? `${item}***` : item).join('')
+}

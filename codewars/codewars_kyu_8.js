58e268ebd021a68a18d9cfd22354ad5551ccebd6
Vika <vikapetrova8@gmail.com>
2024-06-17 23:42:03 +0300

    Sum of differences in array

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1346,3 +1346,21 @@ function isVow(a){
     }
     return a
 }
+
+//Your task is to sum the differences between consecutive pairs in the array in descending order.
+//
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+//
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+function sumOfDifferences(arr) {
+    let sortArr = arr.sort((a, b) => b - a);
+    let res = []
+    for (let i = 0; i < sortArr.length; i++) {
+        if (sortArr[i] !== sortArr[sortArr.length - 1]) {
+            res.push(sortArr[i] - sortArr[i + 1])
+        }
+    }
+    return (arr.length === 0 || arr.length === 1 || arr.every(v => v === arr[0])) ? 0 : res.reduce((a, b) => a+b)
+}

21c8b0f37f3b3d1d57316dae0222ad05c61fcef4
Vika <vikapetrova8@gmail.com>
2024-06-20 23:53:48 +0300

    Get the Middle Character

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -56,3 +56,14 @@ function getCount(str) {
 function oddOrEven(array) {
     return array.length !== 0 ? (array.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd') : 'even'
 }
+
+//You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+//Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+function getMiddle(s) {
+    if (s.length % 2 === 0) {
+        return s[s.length / 2 - 1] + s[s.length / 2]
+    } else {
+        return s[s.length / 2 - 0.5]
+    }
+}

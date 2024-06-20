84bc5d6d8d7fdb3556d04d2f080b83a04c48acb2
Vika <vikapetrova8@gmail.com>
2024-06-20 23:41:53 +0300

    Odd or Even?

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -49,3 +49,10 @@ function XO(str) {
 function getCount(str) {
     return str.split('').filter(el => 'aeiou'.includes(el)).length
 }
+
+//Given a list of integers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+function oddOrEven(array) {
+    return array.length !== 0 ? (array.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd') : 'even'
+}

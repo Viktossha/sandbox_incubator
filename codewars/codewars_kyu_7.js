5559493b885913b11cb149f8d7f1375c2a97158a
Vika <vikapetrova8@gmail.com>
2024-09-12 23:26:00 +0300

    Printer Errors clever

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -125,4 +125,13 @@ function printerError(s) {
     }
 
     return `${res.length}/${s.length}`
+
+    // // your code
+    //     var count = 0;
+    //     for(var i = 0; i < s.length; i++) {
+    //       if (s[i] > "m") {
+    //         count++;
+    //       }
+    //     }
+    //     return count+"/"+s.length;
 }

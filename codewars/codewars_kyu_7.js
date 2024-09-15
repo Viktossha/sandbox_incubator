445d8a1341bf747f8aa1536e809be493eb2a4138
Vika <vikapetrova8@gmail.com>
2024-09-15 21:39:12 +0300

    Two to One

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -172,3 +172,17 @@ function getDivisorsCnt2(n){
 
     return res.length
 }
+
+//Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+//
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+function longest(s1, s2) {
+    let s = s1 + s2
+    return s.split('').filter((el, index) => s.split('').indexOf(el) === index).sort().join('')
+}

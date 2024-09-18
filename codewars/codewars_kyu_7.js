30a7e6f047ea6ce0d22c797a192017855800797f
Vika <vikapetrova8@gmail.com>
2024-09-18 21:54:41 +0300

    Shortest Word

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -212,3 +212,11 @@ String.prototype.toJadenCase = function () {
 function sortByLength (array) {
     return array.sort((a, b) => a.length - b.length)
 }
+
+//Simple, given a string of words, return the length of the shortest word(s).
+//
+// String will never be empty and you do not need to account for different data types.
+function findShort(s){
+    //return s.split(' ').map(el => el.length).sort((a, b) => a - b)[0]
+    return Math.min(...s.split(' ').map(el => el.length))
+}

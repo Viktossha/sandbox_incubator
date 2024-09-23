b3f4931b7369930df1c2bf6a9b7ef10e9db6f056
Vika <vikapetrova8@gmail.com>
2024-09-23 20:13:31 +0300

    Find the stray number

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -281,3 +281,22 @@ function removeSmallest(numbers) {
 
     return res.length !== 0 ? res : []
 }
+
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number.
+// The input array will always be valid! (odd-length >= 3)
+//
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 17, 17, 3, 17] ==> 3
+function stray(numbers) {
+    if (numbers[0] !== numbers[1] && numbers[0] !== numbers[2]) {
+        return numbers[0];
+    }
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] !== numbers[0]) {
+            return numbers[i];
+        }
+    }
+}

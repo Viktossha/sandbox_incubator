323aaa6bc84227dc12285525f87bfe2a414aaec9
Vika <vikapetrova8@gmail.com>
2024-09-24 23:03:04 +0300

    Thinkful - Logic Drills: Traffic light

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1411,3 +1411,17 @@ function greet(name){
 String.prototype.toAlternatingCase = function () {
     return this.split('').map(el => el === el.toLowerCase() ? el.toUpperCase() : el.toLowerCase()).join('')
 }
+
+//You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// For example, when the input is green, output should be yellow.
+function updateLight(current) {
+    switch (current) {
+        case 'green':
+            return 'yellow'
+        case 'yellow':
+            return 'red'
+        case 'red':
+            return 'green'
+    }
+}

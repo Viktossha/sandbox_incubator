0e91acd532cf4327f43a1e75627d6e2a987e5ca3
Vika <vikapetrova8@gmail.com>
2024-09-25 21:46:40 +0300

    Sum of the first nth term of Series

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -300,3 +300,41 @@ function stray(numbers) {
         }
     }
 }
+
+//Task
+// Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
+// You will need to figure out the rule of the series to complete this.
+//
+// Rules
+// You need to round the answer to 2 decimal places and return it as String.
+//
+// If the given value is 0 then it should return "0.00".
+//
+// You will only be given Natural Numbers as arguments.
+//
+// Examples (Input --> Output)
+// n
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+function SeriesSum(n) {
+   // let arr = []
+   //  for (let i = 0; arr.length !== n; i++) {
+   //
+   //          arr.push((1/(1 + i*3)))
+   //  }
+   //  return arr.reduce((a, b) => a + b).toFixed(2)
+
+    if (n === 0) return '0.00'
+    let res = 0
+
+    for (let i = 0; i < n; i++) {
+        res += (1/(1 + i*3))
+    }
+
+    return res.toFixed(2)
+}
+//Формула 1/(1+3*k) описывает последовательность дробей, где каждый следующий член имеет знаменатель, увеличивающийся на 3 по сравнению с предыдущим.
+//k — это индекс или номер элемента последовательности, начиная с 0
+//1 — это числитель каждого элемента последовательности. Все элементы имеют одинаковый числитель.
+//1 + 3k — это знаменатель, который увеличивается с шагом 3 для каждого нового члена последовательности.

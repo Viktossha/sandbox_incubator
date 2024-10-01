aef5f377408c26d61d4af8ba237a0559e9ecad3a
Vika <vikapetrova8@gmail.com>
2024-10-01 22:52:48 +0300

    Testing 1-2-3

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -430,3 +430,22 @@ function checkExam(array1, array2) {
 function squareDigits(num){
     return Number(num.toString().split('').map(el => el**2).join(''))
 }
+
+//Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+//
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+//
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//
+// Examples: (Input --> Output)
+//
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+var number=function(array){
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+        res.push(`${i + 1}: ${array[i]}`)
+    }
+
+    return res.length > 0 ? res : []
+}

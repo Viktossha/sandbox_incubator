8c15be81ce69e14267eb2a7bbb3adc26fbe9a614
Vika <vikapetrova8@gmail.com>
2024-10-06 12:22:42 +0300

    Summing a number's digits

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -476,3 +476,15 @@ function sumTwoSmallestNumbers(numbers) {
     let sortArr = numbers.sort((a, b) => a - b)
     return sortArr[0] + sortArr[1]
 }
+
+//Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+//
+// For example: (Input --> Output)
+//
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+function sumDigits(number) {
+    return Math.abs(number).toString().split('').map(el => Number(el)).reduce((a, b) => a + b)
+}

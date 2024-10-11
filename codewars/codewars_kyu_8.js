d81036b36cac20f27e2a3a9bca084cb824fba85a
Vika <vikapetrova8@gmail.com>
2024-10-11 18:26:51 +0300

    Super Duper Easy

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1449,3 +1449,8 @@ function mouthSize(animal) {
 function take(arr, n) {
     return arr.slice(0, n)
 }
+
+//Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+function problem(x){
+    return typeof x === "string" ? 'Error' : x * 50 + 6
+}

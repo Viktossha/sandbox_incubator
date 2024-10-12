2459518173d96cf96d00245c68afec231bfc4e4c
Vika <vikapetrova8@gmail.com>
2024-10-12 10:48:15 +0300

    Complementary DNA

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -509,3 +509,37 @@ function reverse(string){
 function filter_list(l) {
     return l.filter(el => Number.isInteger(el))
 }
+
+//Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+//
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+//
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+//
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+//
+// Example: (input --> output)
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+function dnaStrand(dna){
+    let arr = dna.split('')
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case 'A':
+                arr[i] = 'T'
+                break
+            case 'T':
+                arr[i] = 'A'
+                break
+            case 'G':
+                arr[i] = 'C'
+                break
+            case 'C':
+                arr[i] = 'G'
+                break
+        }
+    }
+
+    return arr.join('')
+}

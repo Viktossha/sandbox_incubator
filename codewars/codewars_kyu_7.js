5f400b2ffa382f0dbdc3d91dc47d6bd42db3460d
Vika <vikapetrova8@gmail.com>
2024-10-13 10:43:14 +0300

    Fix string case

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -543,3 +543,30 @@ function dnaStrand(dna){
 
     return arr.join('')
 }
+
+//In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+//
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+// For example:
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+function solve(s){
+    let resLow = 0
+    let resUp = 0
+    let arr = s.split('')
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].toLowerCase() === arr[i]) {
+            resLow++
+        } else if (arr[i].toUpperCase() === arr[i]) {
+            resUp++
+        }
+    }
+
+    if (resLow > resUp || resLow === resUp) {
+        return s.toLowerCase()
+    } else {
+        return s.toUpperCase()
+    }
+}

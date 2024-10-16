fc4b227203ac5a67cf277095a777cc9784704055
Vika <vikapetrova8@gmail.com>
2024-10-16 18:52:18 +0300

    Beginner Series #3 Sum of Numbers

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -594,3 +594,45 @@ function solution(nums){
 function openOrSenior(data){
     return data.map(item => item[0] >= 55 && item[1] > 7 ? 'Senior' : 'Open')
 }
+
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+// Note: a and b are not ordered!
+//
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+function getSum(a, b)
+{
+
+    // if (a > b) {
+    //     let arr = [b]
+    //     for (let i = 0; i <= a; i++) {
+    //         arr.push(arr[i] + 1)
+    //     }
+    //     return arr.reduce((a, b) => a + b)
+    // }
+    //
+    // if (a < b) {
+    //     let arr = [a]
+    //     for (let i = 0; i <= b; i++) {
+    //         arr.push(arr[i] + 1)
+    //     }
+    //     return arr.reduce((a, b) => a + b)
+    // }
+    //
+    // if (a === b) {
+    //     return a
+    // }
+
+    //есть формула для суммы последовательных чисел S = (n1+n2)*k / 2
+    let min = Math.min(a, b)
+    let max = Math.max(a, b)
+
+    return ((a + b) * (max - min + 1)) / 2
+}

1402592b587d49b1ca5a1433f3e855e324693696
Vika <vikapetrova8@gmail.com>
2024-10-23 15:31:19 +0300

    Sum of angles

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -702,3 +702,8 @@ function capitalize(s){
 
     return [el1, el2]
 };
+
+//Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+function angle(n) {
+    return 180 * (n - 2)
+}

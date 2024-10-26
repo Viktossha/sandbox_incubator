cbf26931a69308745d724fa3d729fe245a7e5cad
Vika <vikapetrova8@gmail.com>
2024-10-26 12:16:34 +0300

    Determine offspring sex based on genes XX and XY chromosomes

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1474,3 +1474,8 @@ function otherAngle(a, b) {
 var Ball = function(ballType) {
     this.ballType = ballType || 'regular'
 };
+
+// If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+function chromosomeCheck(sperm) {
+    return sperm.split('').includes('Y') ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter."
+}

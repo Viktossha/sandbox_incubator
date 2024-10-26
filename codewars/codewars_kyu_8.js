1ae362f8723f5125c71d719c84c3cc7c1325a07d
Vika <vikapetrova8@gmail.com>
2024-10-26 12:11:48 +0300

    Regular Ball Super Ball

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1461,3 +1461,16 @@ function problem(x){
 function otherAngle(a, b) {
     return 180 - a - b
 }
+
+//Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+//
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+//
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+//
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular'
+};

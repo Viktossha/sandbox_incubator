d81fda3718e755418d37ad5351bfabb22aff3376
Vika <vikapetrova8@gmail.com>
2024-10-30 18:23:16 +0300

    Mumbling

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -763,3 +763,14 @@ function factorial(n){
 function reverseLetter(str) {
     return str.replace(/[^a-zа-я]+/g, '').split('').reverse().join('')
 }
+
+//This time no story, no theory. The examples below show you how to write function accum:
+//
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+function accum(s) {
+    return s.split('').map((el, index) => el.toUpperCase() + el.toLowerCase().repeat(index)).join('-')
+}

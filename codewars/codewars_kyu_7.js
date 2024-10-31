3efbeee0654aa895d4920033383076c9de9f63f3
Vika <vikapetrova8@gmail.com>
2024-10-31 20:54:02 +0300

    JavaScript Array Filter

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -774,3 +774,11 @@ function reverseLetter(str) {
 function accum(s) {
     return s.split('').map((el, index) => el.toUpperCase() + el.toLowerCase().repeat(index)).join('-')
 }
+
+//JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+//
+// The solution would work like the following:
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(el => el % 2 === 0)
+}

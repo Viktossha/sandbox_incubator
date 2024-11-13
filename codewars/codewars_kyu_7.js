7bdc39dbb67014b6e9d978cc82c9d4019281cc31
Vika <vikapetrova8@gmail.com>
2024-11-13 21:27:50 +0300

    Sorted? yes? no? how?

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1060,3 +1060,18 @@ function arithmetic(a, b, operator){
     //   'divide'  : a / b
     // }[operator]);
 }
+
+//Complete the method which accepts an array of integers, and returns one of the following:
+//
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+function isSortedAndHow(array) {
+    let sortAsc = [...array].sort((a, b) => a - b)
+    let sortDesc = [...array].sort((a, b) => b - a)
+
+    return JSON.stringify(array) === JSON.stringify(sortAsc)
+            ? "yes, ascending" : (JSON.stringify(array) === JSON.stringify(sortDesc)
+            ? "yes, descending" : "no")
+}

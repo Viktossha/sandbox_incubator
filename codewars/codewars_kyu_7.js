cd56a3270fad4244beaa91a9bf4b08c2319f81ad
Vika <vikapetrova8@gmail.com>
2024-11-19 23:15:18 +0300

    Small enough? - Beginner

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1157,3 +1157,12 @@ function isTriangle(a,b,c)
 function noOdds( values ){
     return values.filter(el => el % 2 === 0)
 }
+
+//You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+//
+// You can assume all values in the array are numbers.
+function smallEnough(a, limit){
+    let newArr = a.filter(el => el <= limit)
+
+    return newArr.length === a.length
+}

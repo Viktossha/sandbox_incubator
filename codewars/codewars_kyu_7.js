3b6eb633682f3817af3b3645370b877d0283b3ec
Vika <vikapetrova8@gmail.com>
2024-11-20 18:29:26 +0300

    The Coupon Code

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1166,3 +1166,15 @@ function smallEnough(a, limit){
 
     return newArr.length === a.length
 }
+
+//Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+//
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+//
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && new Date(currentDate).getTime() < new Date(expirationDate).getTime()
+}

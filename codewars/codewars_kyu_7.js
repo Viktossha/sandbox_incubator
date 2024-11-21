664df2560a6bd32cb9ab00334d6a685e47d5aa81
Vika <vikapetrova8@gmail.com>
2024-11-21 21:28:33 +0300

    Bumps in the Road

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1178,3 +1178,10 @@ function smallEnough(a, limit){
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     return enteredCode === correctCode && new Date(currentDate).getTime() < new Date(expirationDate).getTime()
 }
+
+//Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+//
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+function bump(x) {
+    return x.split('').filter(el => el === 'n').length <= 15 ? 'Woohoo!' : "Car Dead"
+}

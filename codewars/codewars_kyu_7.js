42927bc92b933be0240fb0273c14c05b7681a091
Vika <vikapetrova8@gmail.com>
2024-12-10 23:06:12 +0300

    Find the vowels

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1243,3 +1243,23 @@ function sumCubes(n){
 
     return sum
 }
+
+//We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+//
+// So given a string "super", we should return a list of [2, 4].
+//
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+function vowelIndices(word){
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'y' ]
+    const arr = word.split('').map((el, index) => vowels.includes(el.toLowerCase()) ? (index + 1) : 0)
+    console.log(arr, 'arr')
+    console.log(arr.filter(el => el !==0))
+    return arr.filter(el => el !==0)
+}

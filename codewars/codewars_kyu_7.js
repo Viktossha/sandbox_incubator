9fcdab55e89b44e8340024274b84fc951e81450c
Vika <vikapetrova8@gmail.com>
2024-12-11 21:14:17 +0300

    Number of Decimal Digits

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1263,3 +1263,10 @@ function vowelIndices(word){
     console.log(arr.filter(el => el !==0))
     return arr.filter(el => el !==0)
 }
+
+//Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+//
+// All inputs will be valid.
+function digits(n) {
+   return n.toString().length
+}

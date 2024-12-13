962772bb8939cdb4962671541729e3d98819e66f
Vika <vikapetrova8@gmail.com>
2024-12-13 22:40:45 +0300

    Pillars

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1756,3 +1756,13 @@ function binToDec(bin) {
 function usdcny(usd) {
     return (usd * 6.75).toFixed(2) + ' Chinese Yuan'
 }
+
+//There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+//
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+function pillars(numPill, dist, width) {
+    return numPill === 1 ? 0 : dist * (numPill - 1) * 100 + width * (numPill - 2)
+}

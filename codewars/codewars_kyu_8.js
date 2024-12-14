f131c103cc8c0e7058cde31bae3443c74833583d
Vika <vikapetrova8@gmail.com>
2024-12-14 22:27:28 +0300

    The 'if' function

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1766,3 +1766,14 @@ function usdcny(usd) {
 function pillars(numPill, dist, width) {
     return numPill === 1 ? 0 : dist * (numPill - 1) * 100 + width * (numPill - 2)
 }
+
+//Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+//
+// When bool is truthy, func1 should be called, otherwise call the func2.
+//
+// Example:
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// // Logs 'True' to the console.
+function _if(bool, func1, func2) {
+    return bool ? func1() : func2()
+}

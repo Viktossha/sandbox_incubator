56943aa04dab3603b56cf026808ecb3bc349f2b2
Vika <vikapetrova8@gmail.com>
2024-12-20 22:52:58 +0300

    Grasshopper - Function syntax debugging

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1855,3 +1855,9 @@ function multiTable(number) {
 
     return res.join('\n')
 }
+
+//Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+function main (verb, noun) {
+    return verb + noun
+}

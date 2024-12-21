1a00f7f5e9c5e1f2e459513b9d81aa8d89916709
user <vikapetrova8@gmail.com>
2024-12-21 12:45:45 +0300

    Sum of Multiples

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1861,3 +1861,23 @@ function multiTable(number) {
 function main (verb, noun) {
     return verb + noun
 }
+
+//Find the sum of all multiples of n below m
+//
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+function sumMul(n,m){
+    if (n < 0 || n === 0 || m < 0) return 'INVALID'
+
+    let sum = 0
+    for (let i = n; i < m; i++) {
+        if (i % n === 0) sum+=i
+    }
+    return sum
+}

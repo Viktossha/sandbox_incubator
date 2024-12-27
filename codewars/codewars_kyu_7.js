ed2f418fd62d1e7181684fef8f8b7931eae1e26b
Vika <vikapetrova8@gmail.com>
2024-12-27 23:40:34 +0300

    Sort the Gift Code

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1355,3 +1355,14 @@ function divCon(x){
 
     return arrNumbers.reduce((a, b) => a+b, 0) - arrStrings.reduce((a, b) => +a+(+b), 0)
 }
+
+// Sort the Gift Code
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+//
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+function sortGiftCode(code){
+    return code.split('').sort().join('')
+}

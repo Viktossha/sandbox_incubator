1ce9f67ee5b1443336f8192e24bc7f3629d63c29
Vika <vikapetrova8@gmail.com>
2025-01-09 18:20:01 +0300

    Simple validation of a username with regex

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1952,3 +1952,13 @@ function equal5(){
 function odds(values){
     return values.filter(el => el % 2 !== 0);
 }
+
+//Write a simple regex to validate a username. Allowed characters are:
+//
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+function validateUsr(username) {
+    return /^[a-z0-9_]{4,16}$/.test(username)
+}

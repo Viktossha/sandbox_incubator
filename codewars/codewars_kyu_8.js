1dccf7348e3ed9430f3f798d5e9038b43bfa2672
Vika <vikapetrova8@gmail.com>
2025-01-11 22:51:42 +0300

    Is it a number?

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -1971,3 +1971,21 @@ function validateUsr(username) {
 function billboard(name, price = 30){
     return name.split('').map(el => el = price).reduce((a, b) => (a + b), 0)
 }
+
+//Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+//
+// Valid examples, should return true:
+//
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+//
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+function isDigit(s) {
+    s = s.trim()
+    return !isNaN(s) && s !== ''
+}

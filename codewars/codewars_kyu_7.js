3b33ea8811bf9a9737108b149003aa45aea89130
Vika <vikapetrova8@gmail.com>
2025-01-12 20:16:38 +0300

    Alphabet symmetry

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1366,3 +1366,31 @@ function divCon(x){
 function sortGiftCode(code){
     return code.split('').sort().join('')
 }
+
+//Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+//
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+//
+// solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// See test cases for more examples.
+//
+// Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+function solve(arr) {
+    return arr.map(el => {
+        let res = 0
+        for (let i = 0; i < el.length; i++) {
+            if (el[i].toLowerCase().charCodeAt(0) - 96 === i + 1) {
+                res++
+            }
+        }
+        return res
+    });
+
+    //console.log('a'.charCodeAt(0)); // 97
+    // console.log('b'.charCodeAt(0)); // 98
+    // console.log('z'.charCodeAt(0)); // 122
+    //
+    // console.log('a'.charCodeAt(0) - 96); // 1
+    // console.log('b'.charCodeAt(0) - 96); // 2
+    // console.log('z'.charCodeAt(0) - 96); // 26
+}

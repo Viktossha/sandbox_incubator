6e858772d61b15bde9ae07d427c24a7cb59600ef
Vika <vikapetrova8@gmail.com>
2025-01-19 19:46:42 +0300

    Classy Classes

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2010,3 +2010,15 @@ function nameShuffler(str){
 function hello(name) {
     return name ? `Hello, ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!` : `Hello, World!`;
 }
+
+// Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get info() {
+        return `${this.name}s age is ${this.age}`;
+    }
+}

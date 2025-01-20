bd3e41aa6ace4d5340a6307e52a368f955a82c67
Vika <vikapetrova8@gmail.com>
2025-01-20 22:36:08 +0300

    Find Nearest square number

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2022,3 +2022,24 @@ class Person {
         return `${this.name}s age is ${this.age}`;
     }
 }
+
+//Your task is to find the nearest square number of a positive integer n. In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+//
+// For example, if n = 111, then the nearest square number equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+//
+// If n is already a perfect square (e.g. n = 144, n = 81, etc.), you need to just return n
+function nearestSq(n){
+    if (Number.isInteger(Math.sqrt(n))) return n
+
+    let a = n
+    while (Number.isInteger(Math.sqrt(a)) === false) {
+        a++
+    }
+
+    let b = n
+    while (Number.isInteger(Math.sqrt(b)) === false) {
+        b--
+    }
+
+    return a - n > n - b ? b : a
+}

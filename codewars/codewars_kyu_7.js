12df65482f5906c728f5d942c1dcd9b491d2ec55
Vika <vikapetrova8@gmail.com>
2025-01-26 21:20:19 +0300

    Build a square

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1486,3 +1486,17 @@ function gps(s, x) {
 
     return Math.floor(Math.max(...a))
 }
+
+//I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+//
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+//
+// +++
+// +++
+// +++
+function generateShape(integer){
+    let str = '+'.repeat(integer)
+
+    return Array(integer).fill(str).join('\n')
+}

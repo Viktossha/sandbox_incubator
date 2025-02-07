e95d463db9135bd3f3f40f783614de918c8d6bda
Vika <vikapetrova8@gmail.com>
2025-02-07 23:40:47 +0300

    Ordered Count of Characters

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1511,3 +1511,20 @@ reverse = function(array) {
     }
     return res
 }
+
+//Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+// Consult the solution set-up for the exact data structure implementation depending on your language.
+// Example:
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+const orderedCount = function (text) {
+    let res = []
+    for (const item of text) {
+        let current = res.find(el => el[0] === item)
+        if (current) {
+            current[1]++
+        } else {
+            res.push([item, 1])
+        }
+    }
+    return res
+}

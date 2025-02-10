a5c976dba1e2b8a8658da57c5c70121283451f55
Vika <vikapetrova8@gmail.com>
2025-02-10 23:02:24 +0300

    Switcheroo

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1528,3 +1528,10 @@ const orderedCount = function (text) {
     }
     return res
 }
+
+//Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+function switcheroo(x){
+    return x.split('').map(el => el === 'a' ? 'b' : (el === 'b' ? 'a' : el) ).join('')
+}

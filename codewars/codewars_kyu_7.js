abfa35cc3556373e6d0aba2cc05d39d1a822438b
Vika <vikapetrova8@gmail.com>
2025-02-13 23:00:32 +0300

    Simple beads count

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1543,3 +1543,13 @@ function switcheroo(x){
 function filterString(value) {
     return +value.split('').filter(el => !isNaN(el)).join('')
 }
+
+//Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+//
+// @ @@ @ @@ @ @@ @ @@ @ @@ @
+//
+// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+function countRedBeads(n) {
+    return n >= 2 ? (n - 1)*2 : 0
+}

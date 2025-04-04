ff7ba28229993244ded50c1d5eff82110d38caf5
Vika <vikapetrova8@gmail.com>
2025-04-04 21:19:08 +0300

    max diff - easy

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1627,3 +1627,17 @@ function nthSmallest(arr, pos){
 function solve(arr) {
     return arr.filter((el, index) => index === arr.lastIndexOf(el))
 }
+
+//You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+//
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+function maxDiff(list) {
+    // let sortArr = list.sort((a, b) => a-b)
+    // return list.length < 2 ? 0 : sortArr[sortArr.length-1] - sortArr[0]
+
+    return list.length < 2 ? 0 : Math.max(...list) - Math.min(...list)
+};

7a4f61584f723d9cdfc51d3f9341235212e600dc
Vika <vikapetrova8@gmail.com>
2025-04-09 23:18:43 +0300

    Strong Number (Special Numbers Series #2)

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1641,3 +1641,34 @@ function maxDiff(list) {
 
     return list.length < 2 ? 0 : Math.max(...list) - Math.min(...list)
 };
+
+//Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+//
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+//
+// Task
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+//
+// Examples
+// 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+// 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+// 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+// 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+function strong(n) {
+    let arr = n.toString().split('')
+    let sum = 0
+
+    for (const item of arr) {
+        sum += factorial(+item)
+    }
+
+    return sum === n ? "STRONG!!!!" : "Not Strong !!" ;
+}
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1
+    } else {
+        return n * factorial(n-1)
+    }
+}

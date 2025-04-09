e62f84ff9ef2f608c0cd33569821e1e4a9146f63
Vika <vikapetrova8@gmail.com>
2025-04-09 23:01:19 +0300

    Basic Training: Add item to an Array

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2245,3 +2245,11 @@ let Ghost = function() {
 function integrate(coefficient, exponent) {
     return `${Math.floor(coefficient/(exponent+1))}x^${exponent+1}`
 }
+
+//Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+//
+// The websites array has already been defined for you using the following code:
+//
+var websites = [];
+websites.push('codewars')

516878a8a6514214660a964d80cd6faa108c94ba
Vika <vikapetrova8@gmail.com>
2025-04-10 22:43:34 +0300

    pick a set of first elements

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2253,3 +2253,18 @@ function integrate(coefficient, exponent) {
 //
 var websites = [];
 websites.push('codewars')
+
+//Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+//
+// If n == 0 return an empty sequence []
+//
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+function first(arr, n=1) {
+    //return n === 0 ? [] : (n === 1 ? [arr[0]] : arr.slice(0, n))
+    return arr.slice(0, n)
+}

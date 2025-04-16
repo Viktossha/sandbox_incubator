8f689dd400a114fa96efa3218afcf9a6a39f637f
Vika <vikapetrova8@gmail.com>
2025-04-16 23:09:19 +0300

    Area of a Square

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2268,3 +2268,9 @@ function first(arr, n=1) {
     //return n === 0 ? [] : (n === 1 ? [arr[0]] : arr.slice(0, n))
     return arr.slice(0, n)
 }
+
+//Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+function squareArea(A){
+    return ((2*A)/Math.PI)**2
+}

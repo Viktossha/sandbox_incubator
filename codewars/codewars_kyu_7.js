c6b7ea8279592d9f199d4e6ab355cd581cc06774
Vika <vikapetrova8@gmail.com>
2025-04-20 17:54:34 +0300

    Simple string characters

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1695,3 +1695,27 @@ function adjacentElementsProduct(array) {
 function min(arr, toReturn) {
     return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr))
 }
+
+//In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters (everything else), as follows.
+// The order is: uppercase letters, lowercase letters, numbers and special characters.
+// "*'&ABCDabcde12345" --> [ 4, 5, 5, 3 ]
+function solve(s){
+    let upper = 0
+    let lower = 0
+    let numbers = 0
+    let special = 0
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            upper++
+        } else if (s[i] >= 'a' && s[i] <= 'z') {
+            lower++
+        } else if (s[i] >= '0' && s[i] <= '9') {
+            numbers++
+        } else {
+            special++
+        }
+    }
+
+    return [upper, lower, numbers, special]
+}

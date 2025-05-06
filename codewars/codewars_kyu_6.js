4cd16273e1366e4c91c53b24096c10e216463e35
Vika <vikapetrova8@gmail.com>
2025-05-06 22:02:50 +0300

    Reverse every other word in the string

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -56,3 +56,11 @@ function dashatize(num) {
 
     return str.join('').replace(/^-+|-+$/g, '')
 }
+
+//Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+function reverse(str) {
+    return str.trim().split(' ').map((el, index) => index % 2 === 0
+        ? el
+        : el.split('').reverse().join(''))
+        .join(' ')
+}

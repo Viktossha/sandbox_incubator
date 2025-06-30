d0bb250bd29af99e660795e02e4b5ac9cb0c55e1
Vika <vikapetrova8@gmail.com>
2025-06-30 21:41:45 +0300

    Stop gninnipS My sdroW!

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -194,3 +194,12 @@ function alphabetWar(fight)
     }
     return resRight > resLeft ? "Right side wins!" : (resRight < resLeft ? "Left side wins!": "Let's fight again!")
 }
+
+//Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+// Examples:
+// "Hey fellow warriors"  --> "Hey wollef sroirraw"
+// "This is a test        --> "This is a test"
+// "This is another test" --> "This is rehtona test"
+function spinWords(string){
+    return string.split(' ').map(el => el.length > 4 ? [...el].reverse().join('') : el).join(' ')
+}

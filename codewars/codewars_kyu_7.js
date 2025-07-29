ae66f55f9b98b250c4cdeb96bfddfa9f65813e7c
Vika <vikapetrova8@gmail.com>
2025-07-29 23:15:03 +0300

    Are the numbers in order?

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1835,3 +1835,12 @@ function twoOldestAges(ages){
    let sortArr = ages.sort((a, b) => b - a)
     return [sortArr[1], sortArr[0]]
 }
+
+//inAscOrder([1,2,4,7,19]); // returns true
+// inAscOrder([1,2,3,4,5]); // returns true
+// inAscOrder([1,6,10,18,2,4,20]); // returns false
+// inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
+function inAscOrder(arr) {
+    let sortArr = [...arr].sort((a, b) => a - b)
+    return sortArr + '' === arr + ''
+}

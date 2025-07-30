cc1bc0cdb5608a8ace4eefb79d69c3af151a63c2
Vika <vikapetrova8@gmail.com>
2025-07-30 21:30:41 +0300

    Leap Years

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1844,3 +1844,12 @@ function inAscOrder(arr) {
     let sortArr = [...arr].sort((a, b) => a - b)
     return sortArr + '' === arr + ''
 }
+
+//In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+//
+// Years divisible by 4 are leap years,
+// but years divisible by 100 are not leap years,
+// but years divisible by 400 are leap years.
+function isLeapYear(year) {
+    return year % 100 !== 0 && year % 4 === 0 || year % 400 === 0;
+}

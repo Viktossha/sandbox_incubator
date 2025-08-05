6b7ac14d6be52119bf469b9e949f670d4c9d9c7d
user <vikapetrova8@gmail.com>
2025-08-05 16:59:06 +0300

    Fizz Buzz

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1324,18 +1324,6 @@ var greet = function(name) {
 // Examples of returns in different languages:
 // a = ["az", "toto", "picaro", "zone", "kiwi"] -->
 // [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]]
-// or
-//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
-// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
-// or
-// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
-// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
-// or
-// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
-// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
-// or
-// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
-// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
 // Note
 // You can see other examples for each language in "Your test cases"
 function partlist(arr) {
@@ -1343,5 +1331,34 @@ function partlist(arr) {
     for (let i = 1; i < arr.length; i++) {
         res.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')])
     }
+    return res
+}
+
+//Return an array containing the numbers from 1 to N, where N is the parametered value.
+//
+// Replace certain values however if any of the following conditions are met:
+//
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+//
+// Method calling example:
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+// Return an array
+function fizzbuzz(n) {
+    let res = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res.push('FizzBuzz')
+        } else if (i % 3 === 0) {
+            res.push('Fizz')
+        } else if (i % 5 === 0) {
+            res.push('Buzz')
+        } else {
+            res.push(i)
+        }
+    }
+
     return res
 }

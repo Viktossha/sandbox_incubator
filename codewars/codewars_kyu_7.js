3ad49d31e2b9c0ae3f7cf78282cc88fbf6597cb3
user <vikapetrova8@gmail.com>
2025-08-14 19:35:27 +0300

    Remove duplicate words

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1373,3 +1373,18 @@ function reverseNumber(n) {
         return Math.abs(res)
     }
 }
+
+//Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+//
+// Example:
+//
+// Input:
+//
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+//
+// Output:
+//
+// 'alpha beta gamma delta'
+function removeDuplicateWords (s) {
+    return s.split(' ').filter((el, index) => index === s.split(' ').findIndex(k => k === el)).join(' ')
+}

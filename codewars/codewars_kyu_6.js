00492b2054b0d2b2a7625166ae667ca612cb7f8b
user <vikapetrova8@gmail.com>
2025-08-17 13:25:43 +0300

    Counting Duplicates

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -289,3 +289,37 @@ function digitalRoot(n) {
 
     return n
 }
+
+//Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+//
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+function duplicateCount(text){
+    if (text.length === 0) return 0
+    const arr = text.toLowerCase().split('')
+    let counts = {}
+    let res = 0
+
+    for (const el of arr) {
+        if (counts[el]) {
+            counts[el] += 1
+        } else {
+            counts[el] = 1
+        }
+    }
+
+    for (const key in counts) {
+        if (counts[key] > 1) {
+            res += 1
+        }
+    }
+
+    return res
+}

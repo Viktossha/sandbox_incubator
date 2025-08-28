741ae413c1b7ec2c019c6ead8c7db71e2cdebff4
user <vikapetrova8@gmail.com>
2025-08-28 09:13:32 +0300

    My Language Skills

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1412,3 +1412,30 @@ function repeats(arr){
     const uniqArr = arr.filter((el) => arr.lastIndexOf(el) === arr.indexOf(el))
     return uniqArr.reduce((a, b) => a+b)
 }
+
+//You are given a dictionary-like object (implementation may vary by language) containing languages as keys and your corresponding test results as values. Return the list of languages where your score is at least 60, in descending order of the scores.
+//
+// Note: the scores will always be unique (so no duplicate values)
+//
+// Examples
+// Input object with key-value pairs:
+// "Java" -> 10, "Ruby" -> 80, "Python" -> 65
+// Ouput:
+// [ "Ruby", "Python" ]
+//
+// Input object with key-value pairs:
+// "Hindi" -> 60, "Greek" -> 71, "Dutch" -> 93
+// Output:
+// [ "Dutch", "Greek", "Hindi" ]
+//
+// Input object with key-value pairs:
+// "C++" -> 50, "ASM" -> 10, "Haskell" -> 20
+// Output:
+// [ ]
+function myLanguages(results) {
+
+    let arrResults = Object.entries(results)
+    let filteredResults = arrResults.filter(([lang, res]) => res >= 60)
+
+    return filteredResults.sort((a, b) => b[1] - a[1]).map(([lang]) => lang)
+}

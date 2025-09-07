499fc480ecfc1e4ae4ef1be0f663fa52b061ce43
Vika <vikapetrova8@gmail.com>
2025-09-07 11:46:42 +0300

    All unique

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1488,3 +1488,11 @@ function solution(digits){
 
     return max
 }
+
+//Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+function hasUniqueChars(str){
+    // const res = str.split('').filter((el, index) => str.indexOf(el) !== index)
+    // return res.length <= 0
+    return new Set(str).size === str.length
+}

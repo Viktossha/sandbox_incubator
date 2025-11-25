059c6953e6a57de73b67a149cabe247f6107b6f2
Vika <vikapetrova8@gmail.com>
2025-11-25 12:31:35 +0300

    Meeting

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -555,3 +555,44 @@ function solve(s) {
 
     return Math.max(...result);
 }
+
+//John has invited some friends. His list is:
+//
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+//
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+//
+// So the result of function meeting(s) will be:
+//
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+function meeting(s) {
+    const arr = s.split(';').map(el => el.toUpperCase().split(':'))
+    const arrSort = arr.sort((a, b) => {
+        if (a[1] > b[1]) {
+            return 1
+        }
+
+        if (a[1] < b[1]) {
+            return -1
+        }
+
+        if (a[1] === b[1]) {
+            if (a[0] > b[0]) {
+                return 1
+            }
+
+            if (a[0] < b[0]) {
+                return -1
+            }
+
+            return 0
+        }
+    })
+
+    return arrSort.map(el => `(${el[1]}, ${el[0]})`).join('')
+
+}

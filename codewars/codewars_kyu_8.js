88001d4b920a23c483e74dde64d0ed3ffb4a7efe
Vika <vikapetrova8@gmail.com>
2025-11-26 13:01:11 +0300

    Switch it Up!

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2345,3 +2345,11 @@ function popElement(arr){
     arr.pop()
     return arr
 }
+
+//When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+// Input: 1
+// Output: "One".
+function switchItUp(number){
+    const numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return numbers[number]
+}

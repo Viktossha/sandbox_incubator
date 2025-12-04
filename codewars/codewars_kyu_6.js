1db45e2af87db79bd966a563d354209f864a99e4
Vika <vikapetrova8@gmail.com>
2025-12-04 23:05:57 +0300

    Kebabize

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -596,3 +596,15 @@ function meeting(s) {
     return arrSort.map(el => `(${el[1]}, ${el[0]})`).join('')
 
 }
+
+//Modify the kebabize function so that it converts a camel case string into a kebab case.
+//
+// "camelsHaveThreeHumps"  -->  "camels-have-three-humps"
+// "camelsHave3Humps"  -->  "camels-have-humps"
+// "CAMEL"  -->  "c-a-m-e-l"
+// Notes:
+//
+// the returned string should only contain lowercase letters
+function kebabize(str) {
+    return str.split('').filter(el => el != +el).map((el, index) => index !== 0 && el.toUpperCase() === el ? `-${el}`.toLowerCase()  : el.toLowerCase()).join('')
+}

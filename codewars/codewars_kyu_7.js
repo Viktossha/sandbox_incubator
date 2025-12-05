5b80ead4a47bbad4885d46ae3caead4afb4b5a1f
Vika <vikapetrova8@gmail.com>
2025-12-05 22:49:31 +0300

    Sort by Last Char

--- codewars/codewars_kyu_7.js
+++ codewars/codewars_kyu_7.js
@@ -1552,3 +1552,18 @@ function menFromBoys(arr){
 function add(n) {
     return (x) => x+n
 }
+
+//Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+// All inputs will be valid.
+function last(x){
+    return x.split(' ').sort((a, b) => {
+        if (a[a.length - 1] < b[b.length - 1]) {
+            return -1
+        }
+        if (a[a.length - 1] > b[b.length - 1]) {
+            return 1
+        }
+        return  0
+    })
+}

c7bb1ab7f7d2c48a7e73899747f493333085785e
Vika <vikapetrova8@gmail.com>
2025-12-12 17:43:21 +0300

    Highest Scoring Word

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -631,3 +631,33 @@ function wave(str){
 
     return res
 }
+
+//Given a string of words, you need to find the highest scoring word.
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+// You need to return the highest scoring word as a string.
+// If two words score the same, return the word that appears earliest in the original string.
+// All letters will be lowercase and all inputs will be valid.
+function high(x){
+    let arrWords = x.split(' ')
+    let obj = {}
+    arrWords.forEach(el => {
+        let res = 0
+        for (let i = 0; i < el.length; i++) {
+            res += el.charCodeAt(i) - 96
+        }
+        return obj[el] = res
+    })
+
+    let result = 0
+    let wordRes
+    for (const word in obj) {
+
+        if (obj[word] > result) {
+            result = obj[word]
+            wordRes = word
+        }
+    }
+
+    return wordRes
+}

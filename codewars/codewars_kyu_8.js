4e63331de003ce3a10a5b008708a5d505852c132
Vika <vikapetrova8@gmail.com>
2025-12-24 22:26:46 +0300

    OOP: Object Oriented Piracy

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2441,3 +2441,38 @@ class Woman extends Human{
         super();
     }
 }
+
+//You begin with writing a generic Ship class / struct:
+//
+// class Ship {
+//   constructor(draft, crew) {
+//     this.draft = draft
+//     this.crew = crew
+//   }
+// }
+// Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+//
+// draft - an estimate of the ship's weight based on how low it is in the water
+// crew - the count of crew on board
+// const titanic = new Ship(15, 10);
+// Task
+// You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+//
+// Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+//
+// Add the method
+//
+// isWorthIt
+// to decide if the ship is worthy to loot. For example:
+//
+// titanic.isWorthIt() // return false
+class Ship {
+    constructor(draft, crew) {
+        this.draft = draft
+        this.crew = crew
+    }
+
+    isWorthIt() {
+        return (this.draft - this.crew * 1.5) > 20
+    }
+}

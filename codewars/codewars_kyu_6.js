be56a40b37802f712bf06d43a70a5bef4408b470
Vika <vikapetrova8@gmail.com>
2025-12-26 00:04:47 +0300

    Delete occurrences of an element if it occurs more than n times

--- codewars/codewars_kyu_6.js
+++ codewars/codewars_kyu_6.js
@@ -732,3 +732,22 @@ function towerBuilder(nFloors) {
     }
     return res
 }
+
+//Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+// For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+// With list [20,37,20,21] and number 1, the result would be [20,37,21].
+function deleteNth(arr,n){
+    let obj = {}
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!obj[arr[i]]) {
+            obj[arr[i]] = 0
+        }
+        obj[arr[i]]++
+        if (obj[arr[i]] <= n) {
+            res.push(arr[i])
+        }
+
+    }
+    return res
+}

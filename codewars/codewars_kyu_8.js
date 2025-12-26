c858f8679ee3da0aea5d01bbf0cb476613f26677
Vika <vikapetrova8@gmail.com>
2025-12-27 02:47:55 +0300

    Fuel Calculator: Total Cost

--- codewars/codewars_kyu_8.js
+++ codewars/codewars_kyu_8.js
@@ -2476,3 +2476,14 @@ class Ship {
         return (this.draft - this.crew * 1.5) > 20
     }
 }
+
+//In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+//
+// Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+function fuelPrice(litres, pricePerLitre) {
+    const step = 0.05
+
+    let discount = Math.floor((litres / 2)) * step
+    if (discount >= 0.25) discount = 0.25
+    return +((pricePerLitre - discount) * litres).toFixed(2)
+}

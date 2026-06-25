2a25556e0a67733770905f4beac2c3fbf1bc7c4b
Vika <vikapetrova8@gmail.com>
2026-06-25 23:59:46 +0300

    Opposites Attract

---
+++ sql/8_kyu_sql.sql
@@ -0,0 +1,6 @@
+/*
+-- # write your SQL statement here: you are given a table 'love' with columns 'flower1' and 'flower2',
+return a table with columns ('flower1' and 'flower2') and your result in a column named 'res'.
+*/
+
+SELECT flower1, flower2, flower1 % 2 <> flower2 % 2 AS res FROM love

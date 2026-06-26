fa12cb947a7bd587904d91818d4607d16a0c3728
Vika <vikapetrova8@gmail.com>
2026-06-26 18:23:40 +0300

    Adults only

--- sql/8_kyu_sql.sql
+++ sql/8_kyu_sql.sql
@@ -30,3 +30,10 @@ name
 country
 */
 SELECT name, country FROM travelers WHERE country NOT IN ('Canada', 'Mexico', 'USA')
+
+/*In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+users table schema
+name
+age
+*/
+SELECT name, age FROM users WHERE age >= 18

de45c069fcc7d86b2a7e3791ca634342cf8ee3ba
Vika <vikapetrova8@gmail.com>
2026-06-26 18:36:23 +0300

    Collect Tuition

--- sql/8_kyu_sql.sql
+++ sql/8_kyu_sql.sql
@@ -51,3 +51,17 @@ You can't attend if the attending column returns anything but true
 */
 INSERT INTO participants (name, age, attending) VALUES ('Vika', 28, true)
 SELECT * FROM participants;
+
+/*
+You are working for a local school, and you are responsible for collecting tuition from students.
+You have a list of all students, some of them have already paid tution and some haven't.
+Write a select statement to get a list of all students who haven't paid their tuition yet. The list should include all the data available about these students.
+students table schema
+name (string)
+age (integer)
+semester (integer)
+mentor (string)
+tuition_received (Boolean)
+*/
+-- SELECT * FROM students WHERE tuition_received = false
+SELECT * FROM students WHERE NOT tuition_received

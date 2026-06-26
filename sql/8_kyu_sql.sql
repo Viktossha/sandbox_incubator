d656ace7a22d057d5e1b68a445429b67119ef2cf
Vika <vikapetrova8@gmail.com>
2026-06-26 18:09:37 +0300

    Remove String Spaces

--- sql/8_kyu_sql.sql
+++ sql/8_kyu_sql.sql
@@ -4,3 +4,6 @@ return a table with columns ('flower1' and 'flower2') and your result in a colum
 */
 
 SELECT flower1, flower2, flower1 % 2 <> flower2 % 2 AS res FROM love
+
+-- # write your SQL statement here: you are given a table 'nospace' with column 'x', return a table with column 'x' and your result in a column named 'res'.
+SELECT x, REPLACE(x, ' ', '') AS res FROM nospace

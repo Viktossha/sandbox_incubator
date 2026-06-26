a541d4b81e1d6402f4194f369e095026afbdfb30
Vika <vikapetrova8@gmail.com>
2026-06-26 18:30:49 +0300

    Register for the Party

--- sql/8_kyu_sql.sql
+++ sql/8_kyu_sql.sql
@@ -37,3 +37,17 @@ name
 age
 */
 SELECT name, age FROM users WHERE age >= 18
+
+/*
+You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+participants table schema
+name (string)
+age (integer)
+attending (boolean)
+NOTES:
+
+Since alcohol will be served, you can only attend if you are 21 or older
+You can't attend if the attending column returns anything but true
+*/
+INSERT INTO participants (name, age, attending) VALUES ('Vika', 28, true)
+SELECT * FROM participants;
